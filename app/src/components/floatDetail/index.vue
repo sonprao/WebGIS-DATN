7f9c19aec353e002d1a6b64b1155a43154fb94d4
nguyen-2020 <nguyen20dang@gmail.com>
2023-06-25 14:53:17 +0700

    feat/17: close popup, bus event on show detail

--- app/src/components/floatDetail/index.vue
+++ app/src/components/floatDetail/index.vue
@@ -13,25 +13,51 @@
           @click="closeCard"
         />
       </div> -->
-      <q-carousel swipeable animated v-model="slideImage" control-color="secondary" arrows navigation infinite
-        ref="carousel" style="height: 200px;">
+      <q-carousel
+        swipeable
+        animated
+        v-model="slideImage"
+        control-color="secondary"
+        arrows
+        navigation
+        infinite
+        ref="carousel"
+        style="height: 200px"
+      >
         <q-carousel-slide :name="1" :img-src="image" />
         <q-carousel-slide :name="2" :img-src="image" />
         <q-carousel-slide :name="3" :img-src="image" />
         <template v-slot:control>
-          <q-carousel-control position="top-left" class="text-white rounded-borders">
-            <q-btn class="absolute shadow-2 closeClass" round color="white" text-color="black" icon="close" size="sm"
-              @click="closeCard" />
+          <q-carousel-control
+            position="top-left"
+            class="text-white rounded-borders"
+          >
+            <q-btn
+              class="absolute shadow-2 closeClass"
+              round
+              color="white"
+              text-color="black"
+              icon="close"
+              size="sm"
+              @click="closeCard"
+            />
           </q-carousel-control>
         </template>
       </q-carousel>
       <q-card-section>
-        <q-btn fab color="primary" icon="place" class="absolute"
-          style="top: 0; right: 12px; transform: translateY(-50%)" />
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%)"
+        />
 
         <div class="row no-wrap items-center">
           <div class="col text-h6 ellipsis">{{ title }}</div>
-          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+          <div
+            class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
+          >
             <q-icon name="place" />
             {{ distanceToMyLocation }}
           </div>
@@ -45,23 +71,45 @@
       </q-card-section>
       <q-separator />
       <q-tabs v-model="detailTab" class="bg-teal text-white">
-        <q-tab v-for="(tab, index) of detailTabList" :key="index" :label="tab.label" :name="tab.component" @click="() => {
-          tabExpanded = true;
-        }
-          " />
-        <q-btn flat dense :icon="tabExpanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'" style="height: 100%"
-          @click="tabExpanded = !tabExpanded" />
+        <q-tab
+          v-for="(tab, index) of detailTabList"
+          :key="index"
+          :label="tab.label"
+          :name="tab.component"
+          @click="
+            () => {
+              tabExpanded = true;
+            }
+          "
+        />
+        <q-btn
+          flat
+          dense
+          :icon="tabExpanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+          style="height: 100%"
+          @click="tabExpanded = !tabExpanded"
+        />
       </q-tabs>
       <q-separator />
       <q-slide-transition>
         <div v-show="tabExpanded">
-          <q-tab-panels v-model="detailTab" animated :keep-alive="true" class="shadow-10 rounded-borders">
-            <q-tab-panel v-for="(tab, index) of detailTabList" :key="index" :name="tab.component" class="panelClass">
+          <q-tab-panels
+            v-model="detailTab"
+            animated
+            :keep-alive="true"
+            class="shadow-10 rounded-borders"
+          >
+            <q-tab-panel
+              v-for="(tab, index) of detailTabList"
+              :key="index"
+              :name="tab.component"
+              class="panelClass"
+            >
               <q-scroll-area
-              style="height: 200px; max-width: 400px; padding: 0 10px;"
-               :thumb-style="thumbStyle"
-               :bar-style="barStyle"
-               >
+                style="height: 200px; max-width: 400px; padding: 0 10px"
+                :thumb-style="thumbStyle"
+                :bar-style="barStyle"
+              >
                 <div v-html="tab.content"></div>
               </q-scroll-area>
             </q-tab-panel>
@@ -89,7 +137,7 @@ import { QBtn } from "quasar";
 import { useQuasar } from "quasar";
 import { i18n } from "boot/i18n.js";
 import { Map, View, Overlay } from "ol";
-
+import { $bus } from "boot/bus.js";
 export default defineComponent({
   name: "FloatDetail",
   props: {
@@ -102,10 +150,14 @@ export default defineComponent({
       type: String,
       default: "https://cdn.quasar.dev/img/chicken-salad.jpg",
     },
+    distance: {
+      type: Number,
+      default: 0,
+    },
     coordinate: {
       type: String,
       default: null,
-    }
+    },
   },
   setup(props, { emit }) {
     const vm = getCurrentInstance().proxy;
@@ -133,10 +185,11 @@ export default defineComponent({
     ]);
     const closeCard = () => {
       console.log("hehe");
+      $bus.emit("close-float-detail", true);
       emit("update:model-value", false);
     };
     const distanceToMyLocation = computed(() => {
-      return "250 m";
+      return props.distance;
     });
     const containerRef = ref(null);
     const renderDynamicComponent = ref(null);
@@ -165,19 +218,19 @@ export default defineComponent({
       ComponentToRender,
       renderDynamicComponent,
       thumbStyle: {
-        right: '4px',
-        borderRadius: '5px',
-        backgroundColor: 'teal',
-        width: '5px',
-        opacity: 0.75
+        right: "4px",
+        borderRadius: "5px",
+        backgroundColor: "teal",
+        width: "5px",
+        opacity: 0.75,
       },
       barStyle: {
-        right: '2px',
-        borderRadius: '9px',
-        backgroundColor: 'teal',
-        width: '9px',
-        opacity: 0.2
-      }
+        right: "2px",
+        borderRadius: "9px",
+        backgroundColor: "teal",
+        width: "9px",
+        opacity: 0.2,
+      },
     };
   },
 });
