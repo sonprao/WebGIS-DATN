50a6a955019919d67f32e25d7751dc133f431826
Sondxm <minhsonnm@gmail.com>
2023-07-06 16:40:41 +0700

    + add Register user

--- app/src/stores/user.js
+++ app/src/stores/user.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import {login, loginGoogle} from 'src/api/user'
+import {login, loginGoogle, register} from 'src/api/user'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
@@ -57,6 +57,25 @@ export const useUserStore = defineStore('user', {
         return {}
       }
 
-    }
+    },
+
+    async registerUser(userData) {
+      try {
+        const responseData = await register({
+          email: userData.email,
+          password: userData.password,
+          profile: userData,
+        })
+        this.user = responseData
+        console.log("SONDXM", this.user);
+        this.isLogin = true
+        return responseData
+      } catch {
+        this.user = {}
+        this.isLogin = false
+        return {}
+      }
+
+    },
   }
 })
